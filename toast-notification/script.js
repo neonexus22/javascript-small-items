c84e1affe875ec45699a80217ea61153042b966b
NishantShah <nishant@genesesolution.com>
2020-11-09 20:37:22 +0545

    toast-notification added

---
+++ toast-notification/script.js
@@ -0,0 +1,19 @@
+const btn = document.getElementById("btn");
+const container = document.getElementById("container");
+
+btn.addEventListener("click", () => {
+  createNotification();
+});
+
+function createNotification(event) {
+  const notify = document.createElement("div");
+  notify.classList.add("toast");
+
+  notify.innerText = "This is a toast message";
+
+  container.appendChild(notify);
+
+  setTimeout(() => {
+    notify.remove();
+  }, 3000);
+}

a51f491eb709415a9a687e219e0f10f8b125a7d6
NishantShah <nishant@genesesolution.com>
2020-11-09 20:15:15 +0545

    hamburger with html css and javascript

---
+++ hamburger/script.js
@@ -0,0 +1,9 @@
+const btn = document.getElementById("btn");
+const nav = document.getElementById("nav");
+
+console.log(btn);
+
+btn.addEventListener("click", () => {
+  nav.classList.toggle("active");
+  btn.classList.toggle("active");
+});
